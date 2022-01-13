7a7319364ba8c8c49cf7850c4722a5acd5951b2e
minjiLee68 <dlalswl2598@gmail.com>
2022-01-14 01:11:28 +0900

    .

---
+++ BountyList/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by 이민지 on 2022/01/14.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
